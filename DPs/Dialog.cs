b93fbf92eaa5bdd61f67f73097b26b752eec321f
nikitasinhal <sinhal.nikita@gmail.com>
2021-09-20 16:41:29 +0800

    * DPs.sln: * Dialog.cs: * DPs.csproj:
    * AssemblyInfo.cs: Add project file

---
+++ DPs/Dialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DPs
+{
+    public abstract class Dialog
+    {
+        public abstract IButton CreateButton();
+        
+        public void Render()
+        {
+            var okButton = CreateButton();
+            okButton.OnClick();
+            Console.WriteLine("");
+        }
+
+
+    }
+    public class WindowsDialog : Dialog
+    {
+        public override IButton CreateButton()
+        {
+            return new WindowsButton();
+        }
+
+    }
+
+    public class WebDialog : Dialog
+    {
+        public override IButton CreateButton()
+        {
+            return new WebButton();
+        }
+
+    }
+}
